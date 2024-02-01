a87c1edf582e7d4fa0bb507af030a90999583eea
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2024-02-01 15:33:57 -0600

    Fixed movement animations and movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float movementSpeed = 2f;
 
     private Rigidbody2D rb;
-
+    private Animator myAnim;
     private Vector2 movementDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     
     void Update()
     {
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        myAnim.SetFloat("moveX", rb.velocity.x);
+        myAnim.SetFloat("moveY", rb.velocity.y);
     }
 
     private void FixedUpdate()
